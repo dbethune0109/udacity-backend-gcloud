458a8c81546b77802334fff74534e04027270256
David Bethune <dbethune1970@gmail.com>
2016-12-16 08:31:26 -0800

    more forms

---
+++ templates/templates/main.py
@@ -0,0 +1,23 @@
+import os
+
+import webapp2
+
+form_html="""
+<form>
+<h2>Add A Food<h2>
+<input type="text" name="food">
+%s
+<button>Add</button>
+</form>
+"""
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class MainPage(Handler):
+    def get(self):
+        self.write(form_html)
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
